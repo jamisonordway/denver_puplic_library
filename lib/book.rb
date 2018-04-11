ddd4a18622f6e4457b417a78610f63b361b76493
JamisonOrdway <jamisonordway@gmail.com>
2018-04-11 09:27:08 -0600

    Adds book initialize and book test

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,11 @@
 class Book
+  attr_reader :book
 
+  def initialize(first, last, title, date)
+    @author_first_name = first
+    @author_last_name = last
+    @title = title
+    @publication_date = date 
+  end
 
 end
--- test/book_test.rb
+++ test/book_test.rb
@@ -1,6 +1,14 @@
 require './test/test_helper'
+require './lib/book'
 
 class BookTest < Minitest::Test
 
+  def setup
+    @book = Book.new('Harper', 'Lee', 'To Kill a Mockingbird', '1960')
+  end
+
+  def test_it_exists
+    assert_instance_of Book, @book
+  end
 
 end
