5edb6608e172317f4b0e06612cff41cd5fbea5b0
JamisonOrdway <jamisonordway@gmail.com>
2018-04-11 09:54:31 -0600

    Implements iteration 1

--- lib/book.rb
+++ lib/book.rb
@@ -11,14 +11,5 @@ class Book
     @publication_date = date
   end
 
-  # def book_information
-  #   @book_info = Hash.new
-  #     @book_info["Author first name"] = @author_first_name
-  #     @book_info["Author last name"] = @author_last_name
-  #     @book_info["Book title"] = @title
-  #     @book_info["Publication date"] = @publication_date
-  #     @book_info
-  # end
-
 
 end
--- test/book_test.rb
+++ test/book_test.rb
@@ -12,7 +12,19 @@ class BookTest < Minitest::Test
   end
 
   def test_it_returns_author_first_name
-    assert_equal "Harper", @book.author_first_name
+    assert_equal 'Harper', @book.author_first_name
+  end
+
+  def test_it_returns_author_last_name
+    assert_equal 'Lee', @book.author_last_name
+  end
+
+  def test_it_returns_title
+    assert_equal 'To Kill a Mockingbird', @book.title
+  end
+
+  def test_it_returns_publication_date
+    assert_equal '1960', @book.publication_date
   end
 
 end
